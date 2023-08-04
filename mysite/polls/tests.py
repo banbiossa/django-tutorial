529a50ee5e56daf0a33f00ccfb0eb5c0bc967f85
shota shimizu <shota.shimizu@caddi.jp>
2023-08-04 19:34:06 +0900

    more tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -15,3 +15,23 @@ class QuestionModelTests(TestCase):
         a_future_time = timezone.now() + datetime.timedelta(days=1)
         future_question = Question(pub_date=a_future_time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions
+        whose pub_date is older than 1 day
+        """
+        old_time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=old_time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions
+        whose pub_date is within the last day
+        """
+        good_time = timezone.now() - datetime.timedelta(
+            hours=23, minutes=59, seconds=59
+        )
+        recent_question = Question(pub_date=good_time)
+        self.assertIs(recent_question.was_published_recently(), True)
