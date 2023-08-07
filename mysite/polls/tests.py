9ccc27772a0b6dbc81d27d2c83737067bf4798b1
shota shimizu <shota.shimizu@caddi.jp>
2023-08-07 16:58:08 +0900

    more detail view tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -108,6 +108,12 @@ class QuestionDetailViewTests(TestCase):
         in the future returns a 404 not found
         """
         future_question = create_question("future", 5)
-        url = reverse("polls:detail", args=(future_question.id))
+        url = reverse("polls:detail", args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_past_questions(self):
+        past_q = create_question("past", -1)
+        url = reverse("polls:detail", args=(past_q.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_q.question_text)
